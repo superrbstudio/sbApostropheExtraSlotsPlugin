bfa514270bf9eaeee79038243867986de4e64f38
Giles Smith <giles@superrb.com>
2012-01-23 15:54:42 +0000

    Open and close FAQs

--- web/js/sbApostropheExtraSlotsPlugin.js
+++ web/js/sbApostropheExtraSlotsPlugin.js
@@ -1,3 +1,54 @@
 function sbApostropheJQueryUITabbedContentSlotStart(tabsId) {
 	$('#' + tabsId).tabs();
 }
+
+var sbApostropheFAQsRegistered = false;
+var sbApostropheFirstHide = true;
+
+function sbApostropheFAQSlotStart() {
+	//this has been done
+	if(sbApostropheFAQsRegistered == true) {
+		return;
+	}
+	
+	sbApostropheFAQsRegistered = true;
+	
+	// hide initial
+	$('.sb-ecom-faq-slot-answer').each(function() {
+		if(sbApostropheFirstHide == true) {
+			sbApostropheFirstHide = false;
+		} else {
+			hideAnswer($(this));
+		}
+	});
+	
+	// listen for clicks on heading
+	$('.sb-ecom-faq-slot-question').click(function() {
+		answer = $(this).parent().find('.sb-ecom-faq-slot-answer');
+		if(answer.hasClass('open')) {
+			hideAnswer(answer);
+		} else {
+			showAnswer(answer);
+		}
+	});
+	
+	function hideAnswer(answer) {
+		answer.slideUp('slow', 'easeInOutCirc', function() {
+			$(this).removeClass('open');
+		});
+	}
+	
+	function showAnswer(answer) {
+		answer.slideDown('slow', 'easeInOutCirc', function() {
+			$(this).addClass('open');
+		});
+	}
+	
+}
+
+function sbApostropheFAQsReset() {
+	$('.sb-ecom-faq-slot-question').unbind('click');
+	sbApostropheFAQsRegistered = false;
+	sbApostropheFirstHide = true;
+	sbApostropheFAQSlotStart();
+}
